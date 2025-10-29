a686b6990a40b2045958f1af44cfb33047229436
jemson26 <jem.lachica@gmail.com>
2025-10-30 03:59:09 +0800

    Update sow_estimator.py

--- sow_estimator.py
+++ sow_estimator.py
@@ -165,7 +165,9 @@ async def estimate_new_carrier(data: NewCarrierEstimateRequest):
     # if data.shipmentScreenString != "":
     if len(data.shipmentScreens) != 0:
         comma_count = data.shipmentScreenString.count(",")
-        line_28 = ( comma_count + 1 ) * 8 if len(data.shipmentScreens) > 0 else 0
+        line_28 = ( comma_count + 1 ) * 8 
+    else
+        line_28 = 0
 
     total_effort = sum([
         line_19, line_20, line_21, line_22, line_23,
